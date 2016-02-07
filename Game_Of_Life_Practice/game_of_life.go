98b548574b2fa32c30ceef79edf1924c2b876cab
Jake Reed <jake32321@yahoo.com>
2016-02-06 23:17:51 -0600

    Created new structs and functions

--- Game_Of_Life_Practice/game_of_life.go
+++ Game_Of_Life_Practice/game_of_life.go
@@ -7,3 +7,20 @@ import (
   "math/rand"
   "time"
 )
+
+//Creates a two dirmensional field
+//Uses struct similar to C
+type Field struct{
+  //Data types declared after????
+  s [][]bool
+  w, h int
+}
+
+//This function will make a new empty field based of the determined width and height.
+func NewField(w, h int) *Field {
+    //Makes a two dimensional array for the field 
+    s := make([][]bool, h)
+    for i := range s{
+
+    }
+}
